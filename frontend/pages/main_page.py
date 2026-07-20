e6038e7d90228e5337b6bc2ebc5d9dc2ce36f69a
SergioZSZ <s.zaballosh@gmail.com>
2026-07-20 22:01:37 +0200

    frontend improvements

--- frontend/pages/main_page.py
+++ frontend/pages/main_page.py
@@ -55,7 +55,7 @@ st.title(
 autores = [
     {"name": "Mateo Campaya-Pinto", "orcid": ""},
     {"name": "Elvira Amador-Dominguez", "orcid": "https://orcid.org/0000-0001-6838-1266"},
-    {"name": "Erick Cede�o", "orcid": "https://orcid.org/0009-0004-4478-6890"},
+    {"name": "Erick Cedeño", "orcid": "https://orcid.org/0009-0004-4478-6890"},
     {"name": "Daniel Garijo", "orcid": "https://orcid.org/0000-0003-0454-7145"},
 ]
 
